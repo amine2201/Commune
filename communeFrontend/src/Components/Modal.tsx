475db213b7e16c00aeb599dc128796f75a20998e
Simo El Arfaoui <simoelarfaoui5@gmail.com>
2023-05-18 11:12:15 +0100

    Adding Link to Modal

--- communeFrontend/src/Components/Modal.tsx
+++ communeFrontend/src/Components/Modal.tsx
@@ -50,7 +50,7 @@ const Modal = ()=>{
                           </div>
         
                           <div className="mt-4">
-                          
+                          <Link to='/Status'>
                             <button
                               type="button"
                               className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
@@ -58,6 +58,7 @@ const Modal = ()=>{
                             >
                               Check Status
                             </button>
+                            </Link>
                           </div>
                         </Dialog.Panel>
                       </Transition.Child>
