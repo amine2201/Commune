461e000c15785f106e1f420ba58dd1d871f64b9c
Simo El Arfaoui <simoelarfaoui5@gmail.com>
2023-05-21 18:14:35 +0100

    Service v1

--- communeFrontend/src/Components/Navbar.tsx
+++ communeFrontend/src/Components/Navbar.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 const NavbarTrue = () => {
   return (
     <>
-      <ul className="font-medium flex flex-col p-6 mt-4 px-10 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-14 md:mt-0 md:border-0 md:bg-white">
+      <ul className="font-medium flex flex-col p-6 mt-4  px-10 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-14 md:mt-0 md:border-0 md:bg-white">
           <li>
             <Link className="rounded px-6 pb-2 pt-2.5 text-black font-medium uppercase leading-normal text-primary transition duration-250 ease-in-out hover:bg-neutral-100 hover:text-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:text-primary-700" to="/Status">Status</Link>
           </li>
@@ -34,9 +34,9 @@ const NavbarFalse = () => {
 }
 const Navbar : React.FC<HeaderProps> =  ({isAuthenticated}) => {
     return (
-      <nav className="border-gray-200">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <img src={logoWizara} className="h-10 w-10 mr-[2rem] mix-blend-multiply " alt="Logo" />
+    <nav className="border-gray-200">
+    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 mr-1">
+    <img src={logoWizara} className="h-10 w-10  mix-blend-multiply " alt="Logo" />
     <div className="hidden w-full md:block md:w-auto" >
       {isAuthenticated ? <NavbarTrue/> : <NavbarFalse/>}
     </div>
--- communeFrontend/src/Components/Service.tsx
+++ communeFrontend/src/Components/Service.tsx
@@ -1,18 +1,28 @@
 import Modal from "./Modal"
 import Navbar from "./Navbar"
-import contract from '../assets/contract.jpg'  
+import contract from '../assets/contract.jpg' 
+import add from '../assets/add.png' 
 import { ChangeEvent, useState , useRef } from "react"
+
+
+interface InputComponentProps {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
 const Service =  () => {
     const [file,setFile] = useState<File>()
     const [clicked,setClicked] = useState<boolean>(false)
     const [checked,setChecked] = useState<boolean>(false)
     const [inputComponent , setInputComponent] = useState<JSX.Element[]>([])
+    const [inputClicked , setInputClicked] = useState<number>(1)
     const AddInputComponent = () => {
         setInputComponent([...inputComponent, <CinInput key={inputComponent.length}/>])
         console.log(inputComponent)
+        setInputClicked(inputClicked => inputClicked + 1);
+
     }
-    const Cinref = useRef<HTMLInputElement>(null)
-    const Labelref = useRef<HTMLLabelElement>(null)
+ 
     const handleChange = (e:ChangeEvent<HTMLInputElement>)=>{
          if (e.target.files) {
       setFile(e.target.files[0]);
@@ -32,26 +42,37 @@ const Service =  () => {
         .then((data) => console.log(data))
         .catch((err) => console.error(err));
         setClicked(true)
+        
     }
-    const handleInputClick = () => {
-        if(Cinref.current)  Labelref.current?.focus()
-    }
-    const CinInput =  () => {
+   
+    
+    
+    const CinInput = () => {
+      const [inputValue, setInputValue] = useState('');
+    
+      const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+        console.log(inputRef.current?.value)
+        
+      };
+      const inputRef = useRef<HTMLInputElement>(null);
         return (
             <div className="relative mb-3" data-te-input-wrapper-init>
             <input
-              onClick={handleInputClick}
-              ref={Cinref}
+              ref={inputRef}
+              placeholder="CIN"
               type="text"
-              className="peer block min-h-[auto] w-full rounded border-0 bg-primary-200 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none text-neutral-600 placeholder:text-neutral-600 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+              value={inputRef.current?.value }
+              className="peer block min-h-[auto] w-full rounded border-0 bg-primary-200 px-3 py-[0.32rem] leading-[1.6] outline-none "
               id="CIN"
+             onChange={handleInputChange}
               />
             <label
-            
-                ref={Labelref}
+            placeholder="CIN"
+              
               htmlFor="CIN"
               className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-              >CIN
+              >
             </label>
           </div>
         
@@ -59,23 +80,21 @@ const Service =  () => {
     return (
         <>
         <Navbar isAuthenticated={true}/>
-
-        <section className="flex flex-col items-center justify-center mt-10 mx-auto md:h-[76vh] px-10  w-[75vh]  bg-white rounded-md ">
+       
+        <section style={{ height: `${80+inputClicked*5}vh`}} className="flex flex-col items-center justify-center  mx-auto md:h-[80vh] px-10  w-[75vh]  bg-white rounded-md ">
             <img className="h-50 w-40 mb-[1.5rem] " src={contract} alt="preview"  />
             <h2 className="font-bold py-5 text-center text-black text-xl">Choisissez votre type de service</h2>
             <div>
             <input id="default-radio-1" type="radio" value="" name="default-radio" onClick={()=>setChecked(!checked)} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500  focus:ring-2 "/>
             <label htmlFor="default-radio-1" className=" text-sm font-medium  text-gray-900 pr-5 pl-2">Legalizaition</label>
-            <input id="default-radio-1" type="radio" value="" name="default-radio" onClick={()=>setChecked(!checked)} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+            <input id="default-radio-1" type="radio" value="" name="default-radio" onClick={()=>setChecked(!checked)} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600  focus:ring-2 "/>
             <label htmlFor="default-radio-1" className="ml-2 text-sm font-medium  text-gray-900 dark:text-gray-300">Certification</label>
 
             </div>
             <h2 className="font-bold py-5 text-center text-black text-xl">Choisissez le nombre de signataires</h2>
-            <CinInput/>
-            {/* <button className="p-5 bg-black justify-end"
-             onClick={AddInputComponent} type="submit">add</button>
+            <CinInput />
+             <img onClick={AddInputComponent} src={add} className="h-7 w-7 opacity-70 hover:opacity-50 relative block left-[4cm] bottom-11" />
              {inputComponent}
-    */}
             <div>
             
             
@@ -95,13 +114,14 @@ const Service =  () => {
              <>
 
              <h2 className="font-bold py-5 text-center text-black text-xl">Choisissez votre fichier</h2>
-             <label htmlFor="file" className=" cursor-pointer bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-8 border border-blue-500 hover:border-transparent rounded" >Select Document
-             <input type="file" id="file" name="file" disabled={(checked)} onChange={handleChange} className=" hidden"  accept=".pdf"/>
+             <label  htmlFor="file" className=" cursor-pointer bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-8 border border-blue-500 hover:border-transparent rounded"  >Select Document
+             <input type="file" id="file" name="file" disabled={!(checked)}  onChange={handleChange} className=" hidden"  accept=".pdf" />
              </label> 
               </>}
            
         
         </section>
+        
         </>
     )
 }
