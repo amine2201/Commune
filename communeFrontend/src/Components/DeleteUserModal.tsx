0e02f56398549bf469ebd9e94bd2c0b2c382d450
Simo El Arfaoui <simoelarfaoui5@gmail.com>
2023-06-25 21:57:41 +0100

    adminDashboard and upload improv

---
+++ communeFrontend/src/Components/DeleteUserModal.tsx
@@ -0,0 +1,81 @@
+
+import { Dialog , Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+
+
+const DeleteUserModal =()=>{ 
+    const [isOpen, setIsOpen] = useState<boolean>(true)
+    const closeModal = () => { setIsOpen(false) ;  window.location.reload();}
+    
+    const onSubmitForm = async (e : React.SyntheticEvent) => {
+        e.preventDefault()
+
+    }  
+    
+    return (
+        <>
+          <div className="fixed inset-0 flex items-center justify-center">
+            
+          </div>
+    
+          <Transition appear show={isOpen} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={closeModal}>
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="fixed inset-0 bg-black bg-opacity-25" />
+              </Transition.Child>
+    
+              <div className="fixed inset-0 overflow-y-auto">
+                <div className="flex min-h-full items-center justify-center p-4 text-center">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0 scale-95"
+                    enterTo="opacity-100 scale-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-95"
+                  >
+                    <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-8 text-left align-middle shadow-xl transition-all ">
+                      <Dialog.Title
+                        as="h2"
+                        className="text-md font-bold  text-gray-900  mb-5 whitespace-nowrap"
+                      >
+                        <svg className="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                        </svg>
+                        Êtes-vous sûr(e) de vouloir supprimer cet utilisateur ?
+                      </Dialog.Title>
+                      <div className="mt-2">
+                      <form className="space-y-4 md:space-y-6" action="#" onSubmit={onSubmitForm} method='post'>
+                       
+                      <div className="flex items-center justify-between">
+                         
+                      </div>
+                      <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Supprimer</button>
+                    
+                  </form>
+                      </div>
+    
+                      <div className="mt-4">
+                      
+                      <button type="submit" className="w-full text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onClick={closeModal}>Annuler</button>
+                     
+                      </div>
+                    </Dialog.Panel>
+                  </Transition.Child>
+                </div>
+              </div>
+            </Dialog>
+          </Transition>
+        </>
+      )
+    }
+export default DeleteUserModal ; 
