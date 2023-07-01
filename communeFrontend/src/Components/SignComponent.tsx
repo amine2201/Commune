cdc9f67d00f3262dc651ea835bc9e8639df7d732
Simo El Arfaoui <simoelarfaoui5@gmail.com>
2023-07-01 19:02:43 +0100

    sign ui 2.0

--- communeFrontend/src/Components/SignComponent.tsx
+++ communeFrontend/src/Components/SignComponent.tsx
@@ -38,9 +38,9 @@ const SignComponent=()=> {
         <Navbar isAuthenticated={true}/>	
         <br/>
         
-        <div className='h-[95vh] w-[75vw] flex flex-col items-center justify-center mx-auto  '>
+        <div className='h-[110vh] w-[75vw] flex flex-col items-center justify-center mx-auto  '>
         <div className='flex flex-row justify-center items-center bg-white/70 rounded-t-xl p-3 '>
-        <h1 className='text-2xl font-bold text-black/60 uppercase tracking-wide mr-4 ml-2'>Signature digitale</h1>
+        <h1 className='text-2xl font-bold text-black/60 uppercase tracking-wide mr-4 ml-2 shadow-sm'>Signature digitale</h1>
         <img className="mix-blend-multiply h-[5rem] w-[5rem] " src={DigitalSignature}/></div>
 	<PdfViewerComponent {...props} />
         <div style={{ display: 'flex' }}>
@@ -49,23 +49,37 @@ const SignComponent=()=> {
          <button
            disabled={!isSigned}
            onClick={handleClickSigner}
-           className="p-2 flex flex-row items-center justify-center rounded-lg bg-neutral-100 shadow-lg cursor-pointer hover:bg-green-200 mt-5 transition-transform duration-250 ease-out transform hover:scale-105 mb-3 border-gray-600/30 border-[1px]"
+           className="p-2 flex flex-row items-center justify-center rounded-lg bg-neutral-100 shadow-lg cursor-pointer hover:bg-green-200 mt-5 transition-transform duration-250 ease-out transform hover:scale-105 mb-3 border-gray-600/30 border-[1px] mr-5"
          >
-           <h1 className="font-bold text-black/60 uppercase tracking-wider text-lg px-4">
+           <h1 className="font-bold text-black/60 uppercase tracking-wider text-md px-3">
              Confirmer la signature
            </h1>
            <img src={accept} className="h-9 w-9 mix-blend-multiply" />
          </button>
+         
+         
          {
         localStorage.getItem('role') === Role.employee && (
-         <button
-          onClick={handleClickRejeter}
-          className="p-1 py-[0.3rem] px-5 flex flex-row items-center justify-center rounded-lg bg-neutral-100 shadow-lg cursor-pointer hover:bg-red-300 mt-5 transition-transform duration-250 ease-out transform hover:scale-105 mb-3 border-gray-600/30 border-[1px]">
-        <h1 className="font-bold text-black/60 uppercase tracking-wider text-lg px-4 py-2">
-                Rejeter la signature
-        </h1>
-        <img  src={cancel} className="  h-10 w-10 mix-blend-multiply ml-5 mr-[-0.4cm]" />
-  </button>
+                <div className="flex flex-row  items-center justify-center">
+                <button
+                  disabled={!isSigned}
+                  onClick={handleClickSigner}
+                  className="p-2 flex flex-row items-center justify-center rounded-lg bg-neutral-100 shadow-lg cursor-pointer hover:bg-green-200 mt-5 transition-transform duration-250 ease-out transform hover:scale-105 mb-3 border-gray-600/30 border-[1px] mr-5"
+                >
+                  <h1 className="font-bold text-black/60 uppercase tracking-wider text-md px-3">
+                    Confirmer la signature
+                  </h1>
+                  <img src={accept} className="h-9 w-9 mix-blend-multiply" />
+                </button>
+                <button
+                 onClick={handleClickRejeter}
+                 className="p-1 py-[0.35rem] px-5 flex flex-row items-center justify-center rounded-lg bg-neutral-100 shadow-lg cursor-pointer hover:bg-red-300 mt-5 transition-transform duration-250 ease-out transform hover:scale-105 mb-3 border-gray-600/30 border-[1px]">
+               <h1 className="font-bold text-black/60 uppercase tracking-wider text-md px-3 py-2">
+                       Rejeter la signature
+               </h1>
+               <img  src={cancel} className="  h-10 w-10 mix-blend-multiply ml-5 mr-[-0.4cm]" />
+         </button>
+                </div>
   )}
 </div>
        
